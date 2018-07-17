6ae884501c04a6394f4f1bdd4f332fd273bf99e8
SMGuellord <muhiyasumba@gmail.com>
2018-07-17 09:47:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/smgue/dvtenrichmenttraining/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.smgue.dvtenrichmenttraining
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
